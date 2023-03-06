2ac9ddc713b652f17cd209020b8e34efcda5f225
Matt Woods <mattwoodsmaths@gmail.com>
2023-03-06 20:08:10 +0100

    netlify issue with named imports

--- src/assets/about/index.js
+++ src/assets/about/index.js
@@ -2,7 +2,7 @@ import dashOne from "./dashOne.png";
 import dashTwo from "./dashTwo.png";
 import dashThree from "./dashThree.png";
 import dashFour from "./dashFour.png";
-import aboutStrava from "../../assets/about/StravaIcon.png"
+import aboutStrava from "./stravaIcon.png"
 import dashTable from './dashTable.png'
 import dashMap from "./dashMap.png"
 import dashGraph from "./dashGraph.png"
