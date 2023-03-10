f0d785b2fb945915b8699c3bde0e5ff4d5c21d9d
Angel <32056957+angelsol299@users.noreply.github.com>
2023-03-10 20:04:40 +0100

    adding a static image and create an array with all its propertie so we itarate and keep only one component on the render

--- src/components/About/AboutBlock.tsx
+++ src/components/About/AboutBlock.tsx
@@ -1,45 +1,55 @@
-import {AnimationControls, motion} from "framer-motion"
-import { framerImageVariants, framerTextVariants } from "../../pages/About/utils/FramerVaraints"
+import { AnimationControls, motion } from "framer-motion";
+import {
+  framerImageVariants,
+  framerTextVariants,
+} from "../../pages/About/utils/FramerVaraints";
 
 interface AboutBlockProps {
-  distance: string,
-  block: (node?: Element | null | undefined) => void,
-  animation: AnimationControls,
-  img: string,
-  textAnimation: AnimationControls,
-  children: React.ReactNode,
-  className?: string
+  distance: string;
+  block: (node?: Element | null | undefined) => void;
+  animation: AnimationControls;
+  img: string;
+  textAnimation: AnimationControls;
+  children: React.ReactNode;
+  className?: string;
 }
 
-
-const AboutBlock = ({distance, block,  animation, img, textAnimation, children, className ="timeline--image timeline--image--table"}: AboutBlockProps) => {
+const AboutBlock = ({
+  distance,
+  block,
+  animation,
+  img,
+  textAnimation,
+  children,
+  className = "timeline--image timeline--image--table",
+}: AboutBlockProps) => {
   return (
-    <>        
-    <div className="circle">{distance}</div>
-    <div className="timeline--element"> 
-      <div className="timeline--line--container">
-        <div className="timeline--line"></div>
-      </div>
-          <div ref={block} className="timeline--block">
-            <motion.img
-              className={className}
-              variants={framerImageVariants}
-              initial="visible"
-              animate={animation}
-              src={img}
-            />
-            <motion.div
-              className="timeline--text"
-              animate={textAnimation}
-              variants={framerTextVariants}
-              initial="visible"
-            >
-              {children}
-            </motion.div>
-          </div>
+    <>
+      <div className="circle">{distance}</div>
+      <div className="timeline--element">
+        <div className="timeline--line--container">
+          <div className="timeline--line"></div>
         </div>
+        <div ref={block} className="timeline--block">
+          <motion.img
+            className={className}
+            variants={framerImageVariants}
+            initial="visible"
+            animate={animation}
+            src={img}
+          />
+          <motion.div
+            className="timeline--text"
+            animate={textAnimation}
+            variants={framerTextVariants}
+            initial="visible"
+          >
+            {children}
+          </motion.div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AboutBlock
+export default AboutBlock;
