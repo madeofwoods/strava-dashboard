88a3ca063a480ebbac0795b1e3cbc6cef193078e
Matt Woods <mattwoodsmaths@gmail.com>
2024-02-15 16:50:49 +0100

    grid for large screen

--- src/pages/Maps/LargeMap.tsx
+++ src/pages/Maps/LargeMap.tsx
@@ -1,20 +1,20 @@
-import Maps from "../../components/Maps/Maps"
-import "./LargeMap.css"
-import { useContext, useEffect } from "react"
-import { DataContext } from "../../context/DataContextProvider"
-import demoData from "../../assets/data.json"
+import Maps from "../../components/Maps/Maps";
+import "./LargeMap.css";
+import { useContext, useEffect } from "react";
+import { DataContext } from "../../context/DataContextProvider";
+import demoData from "../../assets/data.json";
 
 export default function LargeMap() {
-  const { stravaDataKey, activeKey } = useContext(DataContext)
-  const [data] = stravaDataKey
-  const [, setActive] = activeKey
+  const { stravaDataKey, activeKey } = useContext(DataContext);
+  const [data] = stravaDataKey;
+  const [, setActive] = activeKey;
 
   useEffect(() => {
-    setActive("Maps")
-  }, [])
+    setActive("Maps");
+  }, []);
   return (
     <div className="large--map widget">
-        <Maps zoom={13} data={data.length > 0 ? data : demoData} />
+      <Maps zoom={13} data={data.length > 0 ? data : demoData} />
     </div>
-  )
+  );
 }
